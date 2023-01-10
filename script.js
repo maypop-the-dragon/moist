8dd17e83fcb55a1f27435c4e1c37a46d3d5b8de8
Blaineworld <blainephargett@gmail.com>
2023-01-10 12:18:55 -0600

    cool button and input styles
    In this commit, I renamed announcements.js to custom.js, fixed a spelling mistake in the readme, wrote a bit of data, and created an awesome moisty button and input style.

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 'use strict';
 
-const DRINK_WEIGHTS = { "water": 1.0 };
+class Drink {
+	// These properties are explained in the constructor's comment.
+	name = "rocks";
+	hydrationFactor = 0;
+	color = "#999";
+
+	/**
+	 * @desc Instances of this class represent drink types.
+	 * They are all stored inside the DRINKS array.
+	 * @param {*} name The drink's name.
+	 * @param {*} hydrationFactor The fraction of the drink that is water.
+	 * @param {*} color The drink's color on the bottle thingy.
+	 */
+	constructor(name, hydrationFactor, color) {
+		this.name = name;
+		this.hydrationFactor = hydrationFactor;
+		this.color = color;
+	}
+};
+
+const DRINKS = [
+	new Drink("water", 1.00, "#9CF"),
+	new Drink("juice", 0.85, "#C03"),
+	new Drink("soda", 0.9, "#421")
+];
